fa57df168b0701efeced94cd558955db8c735ca6
Isaac Simon <ijsimon@sbcglobal.net>
2017-07-18 14:46:13 -0500

    hope tihs works

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,28 @@
-angular.module('blocJams', []);
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+              });
+
+    $stateProvider
+        .state('landing', {
+            url: '/',
+            templateUrl: '../app/template/landing.html'
+          })
+        .state('album', {
+          url: '/',
+          templateUrl: '../app/template/album.html'
+        })
+        .state('collection', {
+          url: "/",
+          templateUrl: '../app/template/collection.html'
+        });
+    }
+
+angular.module('blocJams', ['ui.router']);
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
