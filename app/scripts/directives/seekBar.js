bb8dd396bf044f9d009c1f2c35b81aeccf850232
Isaac Simon <ijsimon@sbcglobal.net>
2017-07-25 16:41:01 -0500

    post checkpoint11 2

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -66,6 +66,7 @@
                       });
                   };
 
+
                   var notifyOnChange = function(newValue) {
                       if (typeof scope.onChange === 'function') {
                           scope.onChange({value: newValue});
