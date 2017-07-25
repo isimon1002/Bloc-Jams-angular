3bd91280b6959d5570da202e8b3265972fbf5f0a
Isaac Simon <ijsimon@sbcglobal.net>
2017-07-25 15:17:11 -0500

    checkpoint 11

---
+++ app/scripts/filters/timecode.js
@@ -0,0 +1,21 @@
+(function() {
+  function timecode() {
+
+    return function(seconds) {
+        var time = Number.parseFloat(seconds);  //163.38
+
+        if (Number.isNaN(time)) {
+            return '-:--';
+          }
+
+        var output = buzz.toTimer(time); //2:43
+
+        return output;
+    };
+
+  }
+
+  angular
+    .module('blocJams')
+    .filter('timecode', timecode);
+})();
